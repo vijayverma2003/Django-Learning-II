273c4b696eb48bec96f632455810d5f1cde53eaf
Vijay <vijayvermakvp@gmail.com>
2022-09-22 11:42:44 +0530

    Initialize Django II

---
+++ store/views.py
@@ -0,0 +1,16 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Product
+from .serializers import ProductSerializer
+
+
+@api_view()
+def product_list(request):
+    return Response('OK')
+
+
+@api_view()
+def product_detail(request, id):
+    product = Product.objects.get(pk=id)
+    serializer = ProductSerializer(product)
+    return Response(serializer.data)
