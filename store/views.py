e2c0982f1086b47ee5b303a588e77a288d64c993
Vijay <vijayvermakvp@gmail.com>
2022-09-22 11:48:23 +0530

    Adding Serializers to views.py

--- store/views.py
+++ store/views.py
@@ -1,16 +1,20 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 from .models import Product
 from .serializers import ProductSerializer
 
 
 @api_view()
 def product_list(request):
-    return Response('OK')
+    queryset = Product.objects.all()
+    serializer = ProductSerializer(queryset, many=True)
+    return Response(serializer.data)
 
 
 @api_view()
 def product_detail(request, id):
-    product = Product.objects.get(pk=id)
+    product = get_object_or_404(Product, pk=id)
     serializer = ProductSerializer(product)
     return Response(serializer.data)
